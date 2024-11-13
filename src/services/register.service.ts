475b81e1f59566578b83112ebc29d2b3d437599a
Norberto Boa <nboa.26@gmail.com>
2024-11-13 23:41:13 +0200

    test: test register service

--- src/services/register.service.ts
+++ src/services/register.service.ts
@@ -1,30 +1,42 @@
 import type { UsersRepository } from "@/repositories/users-repository";
 import { hash } from "bcryptjs";
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
+import type { User } from "@prisma/client";
 
 interface RegisterServiceRequest {
-  name: string;
-  email: string,
-  password: string
+	name: string;
+	email: string;
+	password: string;
+}
+
+interface RegisterServiceResponse {
+	user: User;
 }
 
 export class RegisterService {
+	constructor(private userRepository: UsersRepository) {}
 
-  constructor(private userRepository: UsersRepository) { }
+	async execute({
+		name,
+		email,
+		password,
+	}: RegisterServiceRequest): Promise<RegisterServiceResponse> {
+		const passwordHash = await hash(password, 6);
 
-  async execute({ name, email, password }: RegisterServiceRequest) {
-    const passwordHash = await hash(password, 6);
+		const userWithSameEmail = await this.userRepository.findByEmail(email);
 
-    const userWithSameEmail = await this.userRepository.findByEmail(email);
+		if (userWithSameEmail) {
+			throw new UserAlreadyExistsError();
+		}
 
-    if (userWithSameEmail) {
-      throw new UserAlreadyExistsError()
-    }
+		// const prismaUserRepository = new PrismaUsersRepository();
 
-    // const prismaUserRepository = new PrismaUsersRepository();
+		const user = await this.userRepository.create({
+			name,
+			email,
+			password: passwordHash,
+		});
 
-    const user = await this.userRepository.create({ name, email, password: passwordHash });
-
-    return user;
-  }
+		return { user };
+	}
 }
---
+++ src/services/register.servive.spec.ts
@@ -0,0 +1,40 @@
+import { describe, it, expect } from "vitest";
+import { RegisterService } from "./register.service";
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { compare } from "bcryptjs";
+import { randomUUID } from "crypto";
+
+describe("Register Service", () => {
+	it("Should hash user password upon registartion", async () => {
+		const prismaUsersRepository = new PrismaUsersRepository();
+		const registerService = new RegisterService({
+			async findByEmail(email) {
+				return null; // User does not exist in the database.
+			},
+
+			async create(data) {
+				return {
+					id: randomUUID(),
+					name: data.name,
+					email: data.email,
+					password: data.password,
+					created_at: new Date(),
+					updated_at: new Date(),
+				};
+			},
+		});
+
+		const { user } = await registerService.execute({
+			name: "John Doe",
+			email: "john.doe@example.com",
+			password: "password123",
+		});
+
+		const isPasswordCorrectlyHashed = await compare(
+			"password123",
+			user.password,
+		);
+
+		expect(isPasswordCorrectlyHashed).toBe(true);
+	});
+});
